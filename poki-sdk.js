d806f7aa6f39d05213687292dbc6eb0e4e4cf500
Taharat <sad242-35-052@diu.edu.bd>
2026-06-02 03:23:05 +0600

    Add files via upload

---
+++ poki-sdk.js
@@ -0,0 +1,22 @@
+// Local PokiSDK stub – no ads, no tracking
+window.PokiSDK = {
+  init: () => Promise.resolve(),
+  initWithVideoHB: () => Promise.resolve(),
+  gameLoadingStart: () => {},
+  gameLoadingFinished: () => {},
+  gameInteractive: () => {},
+  gameplayStart: () => {},
+  gameplayStop: () => {},
+  commercialBreak: () => Promise.resolve(),
+  rewardedBreak: () => Promise.resolve(false),
+  setDebug: () => {},
+  logError: () => {},
+  happyTime: () => {},
+  muteAd: () => {},
+  setPlayerAge: () => {},
+  togglePlayerAdvertisingConsent: () => {},
+  sendHighscore: () => {},
+  getLeaderboard: () => Promise.resolve(),
+  getSharableURL: () => Promise.reject(),
+  getURLParam: () => ""
+};
